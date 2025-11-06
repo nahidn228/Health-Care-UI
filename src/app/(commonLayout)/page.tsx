0e815dfd81116ff7c1ef38d69b855f7b51a0de4f
nahidn228 <nahidn228@gmail.com>
2025-11-06 21:06:24 +0600

    added specialities top doctor step and testimonials

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,10 +1,17 @@
 import { Hero } from "@/components/modules/home/Hero";
-
+import Specialities from "@/components/modules/home/Specialties";
+import Steps from "@/components/modules/home/Steps";
+import Testimonials from "@/components/modules/home/Testimonials";
+import TopRatedDoctors from "@/components/modules/home/TopRatedDoctors";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Specialities />
+      <TopRatedDoctors />
+      <Steps />
+      <Testimonials />
     </div>
   );
 }
